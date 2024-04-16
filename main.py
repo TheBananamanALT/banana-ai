27569e24b80406bb94e30997b8462cf3b8fca8b8
scemer <109444031+TheBananamanALT@users.noreply.github.com>
2024-04-16 12:24:29 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import time
+userInput = input("Hi! My name is BananaAI Model x35y2 (the instant text-based A.I). How are you today?: ")
+
+if userInput == "Good" or userInput == "good" or userInput =="Nice" or userInput == "nice":
+    print("That's good!")
+    time.sleep(1)
+userInput2 = input('How may I help today? (try something like a cake recipe)')
+
+if userInput2 == "Cake recipe" or userInput2 == "cake Recipe" or userInput2 == "cake recipe" or userInput2 == "Cake Recipe":
+   print("Try going here: https://www.rainbowtrust.org.uk/get-involved/fundraise/great-rainbow-bake/chocolate-cake-recipe?gad_source=1&gclid=EAIaIQobChMIiun-14PHhQMVEFxHAR3jIgcrEAAYASAAEgKtePD_BwE") 
+else: print('Sorry, but I do not understand.')
